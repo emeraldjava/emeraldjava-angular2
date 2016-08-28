687a1f7480b617305358b362a4317fbf3c1ba6e7
Paul O'Connell <paul.t.oconnell@gmail.com>
2016-08-28 12:32:07 +0100

    Use templateUrl for home page html.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
- 
+
 import { AppComponent }  from './app.component';
+import { Logger } from './logger.service'
 
 @NgModule({
   imports:      [ BrowserModule ],
+	providers:    [ Logger ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
