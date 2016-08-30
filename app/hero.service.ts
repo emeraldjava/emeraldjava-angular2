34e154d618871d5ca7d9d68429c41316456b1185
Paul O'Connell <paul.t.oconnell@gmail.com>
2016-08-30 21:45:58 +0100

    It's not the HeroService list.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,6 +11,12 @@ export class HeroService {
     //  this.logger.log(`Fetched ${heroes.length} heroes.`);
     //  this.heroes.push(...heroes); // fill cache
     //});
-    return heroes;
+		this.heroes = [
+	    new Hero(3, 'SWindstorm'),
+	    new Hero(33, 'SBombasto'),
+	    new Hero(35, 'SMagneta'),
+	    new Hero(30, 'STornado')
+	  ];
+    return this.heroes;
   }
 }
