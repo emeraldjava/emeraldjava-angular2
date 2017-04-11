ff2574e745f5bd8e62a3d5e01b1f77e0367b94f9
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-04-11 09:48:59 +0100

    Add js and js.map files

--- angular.io/src/app/hero.service.ts
+++ angular.io/src/app/hero.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 
 // https://angular.io/docs/ts/latest/tutorial/toh-pt4.html
 @Injectable()
 export class HeroService {
+
+  private  heroes_url = 'api/heroes';
+
+  constructor(private http: Http){}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroes_url)
+        .toPromise()
+        .then(response => response.json().data as Hero[])
+        .catch(this.handleError);
+    //return Promise.resolve(HEROES);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('error',error);
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: number): Promise<Hero> {
