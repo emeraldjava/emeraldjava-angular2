983653ac3d657c5c521031dadc5a0aa1d6757393
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-04-11 09:35:12 +0100

    Add in-memory database

--- angular.io/src/app/app.module.ts
+++ angular.io/src/app/app.module.ts
@@ -1,6 +1,11 @@
-import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+
+// in-memory components
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService}   from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -12,8 +17,10 @@ import { AppRoutingModule }     from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
